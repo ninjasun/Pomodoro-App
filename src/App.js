5bc4f073c799cb05f5a93bdf2d1457a21130d2df
ninjasun07 <davide.mezzetti@hotmail.it>
2018-04-08 16:20:35 +0200

    added button html

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            display:'25:00'
+        }
+        this.onClickReset = this.onClickReset.bind(this);
+        this.onClickStop = this.onClickReset.bind(this);
+        this.onClickReset = this.onClickReset.bind(this);
+    }
+    onClickStart(){
+        //start count down
+
+    }
+    onClickStop(){
+        //stop count down
+
+    }
+    onClickReset(){
+        //reset count down to the original time
+
+    }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +32,34 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+            <div className='display'>
+                {this.state.display}
+            </div>
+            <div className='button-bar'>
+                <button
+                    className='button btn-start'
+                    onClick={this.onClickStart}
+                >
+                    START
+                </button>
+
+                <button
+                    className='button btn-stop'
+                    onClick={this.onClickStop}
+                >
+                    STOP
+                </button>
+
+                <button
+                    className='button btn-reset'
+                    onClick={this.onClickReset}
+                >
+                    RESET
+                </button>
+            </div>
+
+        </div>
       </div>
     );
   }
