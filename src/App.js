737703d0538693db1333bb96daf26bd6b3a6b2bc
ninjasun07 <davide.mezzetti@hotmail.it>
2018-04-08 19:03:55 +0200

    adding countdown timer

--- src/App.js
+++ src/App.js
@@ -6,26 +6,63 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            display:'25:00'
-        }
+            interval:'',
+            timer:{
+                start:1500,
+                end:0,
+                elapsed:1500
+            }
+        };
+
         this.onClickReset = this.onClickReset.bind(this);
-        this.onClickStop = this.onClickReset.bind(this);
-        this.onClickReset = this.onClickReset.bind(this);
+        this.onClickStop = this.onClickStop.bind(this);
+        this.onClickStart = this.onClickStart.bind(this);
     }
+
     onClickStart(){
         //start count down
+        let self = this;
+        console.log("this is: ", this);
+
+        var countDown = function(){
+            console.log("Elapsed is: ", self.state.timer.elapsed);
+            let newElapsed = self.state.timer.elapsed -1;
+
+            self.setState({
+                timer:{
+                    elapsed:newElapsed
+                }
+            });
+        };
+       let interval = setInterval(
+           countDown, 1000
+       );
+
+
+        self.setState({
+            interval:interval
+        })
+
 
     }
     onClickStop(){
         //stop count down
-
+        const self= this;
+        this.setState({
+            interval: clearInterval(self.state.interval)
+        })
     }
+
     onClickReset(){
         //reset count down to the original time
-
+        this.setState({
+            elapsed:1500
+        })
     }
 
   render() {
+
+        console.log("Elapsed is: ", this.state.timer.elapsed)
     return (
       <div className="App">
         <header className="App-header">
@@ -34,7 +71,7 @@ class App extends Component {
         </header>
         <div className="container">
             <div className='display'>
-                {this.state.display}
+                {this.state.timer.elapsed}
             </div>
             <div className='button-bar'>
                 <button
